6a82fc453c6f82cf2289aae35a2c723112f25149
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2022-01-06 21:40:01 +0300

    Updated hero section.

--- portfolio/src/components/Hero/Hero.js
+++ portfolio/src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+    <Section row nopadding>
+        <LeftSection>
+            <SectionTitle main center>
+                Hoş geldin! <br/>
+                ben Erdem Alptuğ Öksüz
+            </SectionTitle>
+            <SectionText>
+                Hayalleri ve idealleri olan bir Türk genciyim. Kendimi 5 yıl sonra sizin karşınızda değil, sizi benim karşımda görüyorum.
+            </SectionText>
+            <Button onClick={() => window.location = "https://google.com"}>Daha fazlası</Button>
+        </LeftSection>
+    </Section>
 );
 
 export default Hero;
