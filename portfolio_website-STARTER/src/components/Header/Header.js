41c09e80489b968c2764a94b0cc16e2a27039089
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2022-01-06 21:20:29 +0300

    Modified portfolio setup files.

---
+++ portfolio_website-STARTER/src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+
+const Header = () =>  (
+  <Container>
+      <Div1>
+          <Link href="/">
+              <a style={{ display: "flex", alignItems: "center", color: 'white', marginBottom:"20px"}} >
+                  <DiCssdeck size="3rem" /> <span>Portfolyo</span>
+              </a>
+          </Link>
+      </Div1>
+      <Div2>
+          <li>
+              <Link href="#projects">
+                  <NavLink>Projelerim</NavLink>
+              </Link>
+          </li>
+          <li>
+              <Link href="#tech">
+                  <NavLink>Teknolojiler</NavLink>
+              </Link>
+          </li>
+          <li>
+              <Link href="#about">
+                  <NavLink>Hakkımda</NavLink>
+              </Link>
+          </li>
+      </Div2>
+      <Div3>
+          <SocialIcons href="https://github.com/erdemalptugoksuz">
+              <AiFillGithub size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com/in/erdemalptugoksuz">
+              <AiFillLinkedin size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://instagram.com/erdemalptugoksuz">
+              <AiFillInstagram size="3rem"/>
+          </SocialIcons>
+      </Div3>
+  </Container>
+);
+
+export default Header;
